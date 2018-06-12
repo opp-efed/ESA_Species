db4c3987ba537616327a612e45c2939d19e449ca
Jennifer Connolly <je.connolly@gmail.com>
2018-06-12 16:42:02 -0400

    Updated the a/b splits to in could HI and the new NHDplus data downloaded Fall 2017. Also updated the SOP to indicated changes

--- A_Bins/Step_A1_splitHUC2_A_B.py
+++ A_Bins/Step_A1_splitHUC2_A_B.py
@@ -4,23 +4,29 @@ import pandas as pd
 
 import arcpy
 
-outlocation = 'L:\Workspace\ESA_Species\Step3\ToolDevelopment\AquModeling\SpatialJoins'
-precip_points = r'L:\Workspace\ESA_Species\Step3\ToolDevelopment\AquModeling\XYprcp_ann_in_project.shp'
-outtables = r'L:\Workspace\ESA_Species\Step3\ToolDevelopment\AquModeling\SpatialJoins\tables_HUC2'
-list_HUC2 = ['10L', '10U', '11', '12', '15', '16', '17', '18']
+outlocation = 'F:\AquaticTables_ESA\Spring 2018\Update_AB_splits\SpatialJoins'
+precip_points = r'F:\AquaticTables_ESA\Spring 2018\XYprcp_ann_in_project_20180611.shp'
+split_list = ['10', '11', '12', '15', '16', '17', '18', '20']  # HUC2 with an a/b split and HUC12 (not HUC12 in AK)
+
+# Be sure the HUC12 file include all area within US jurisdiction; island in HI, AS, CNMI and AK are not in seamless
+# data set and manually added for ESA; data was also loaded into the F:\AquaticTables_ESA\Spring 2018 folder
+HUC12_path = r'D:\ESA\NHDPlusNationalData\InfoAddedForESA\FilesAppended_ESA.gdb\HUC12_Merge'
+
+outtables = outlocation + os.sep + 'tables_HUC2'
 
 
 def create_gdb(out_folder, out_name, outpath):
     if not arcpy.Exists(outpath):
         arcpy.CreateFileGDB_management(out_folder, out_name, "CURRENT")
 
+
 start_time = datetime.datetime.now()
 print "Start Time: " + start_time.ctime()
 
 # location of out files
-outgdb = outlocation + os.sep + 'FirstLoop.gdb' # HUC12 with one or more points directly inside
-no_precip_point = outlocation + os.sep + 'no_precip.gdb' # HUC12 without any points
-closest_point = outlocation + os.sep + 'closest.gdb' # the results of the one to many join HUC12 without point to closest point
+outgdb = outlocation + os.sep + 'FirstLoop.gdb'  # HUC12 with one or more points directly inside
+no_precip_point = outlocation + os.sep + 'no_precip.gdb'  # HUC12 without any points
+closest_point = outlocation + os.sep + 'closest.gdb'  # results of the one to many join to closest point; no pnt within
 
 # Create out locations
 if not os.path.exists(outgdb):
@@ -32,67 +38,82 @@ if not os.path.exists(closest_point):
 
 arcpy.MakeFeatureLayer_management(precip_points, "pnt_lyr")
 
-for value in list_HUC2:
-    print value
-    # out files, csv name with all values by HUC12, outfc from Spatial Join, out fc of HUC12 w/o point
-    outcsv = outtables + os.sep + "HUC2_" + value + '.csv'
-    outfc = outgdb + os.sep + "HUC2_" + value + '_SJ'
-    no_pnt = no_precip_point + os.sep + "HUC2_" + value + '_noPoint'
+# out files, csv name with all values by HUC12, outfc from Spatial Join, out fc of HUC12 w/o point
+outcsv = outtables + os.sep + "HUC2.csv"
+outfc = outgdb + os.sep + "HUC2_SJ"
+no_pnt = no_precip_point + os.sep + "HUC2_noPoint"
 
-    # in location WBD shape
-    HUC12_path = r'L:\NHDPlusV2\NHDPlus{0}\WBDSnapshot\WBD\WBD_Subwatershed.shp'.format(value)
+# FILTER SEAMLESS DATASET TO JUST HUC2s with a/b
+arcpy.Delete_management("HUC_lyr")
+arcpy.MakeFeatureLayer_management(HUC12_path, "HUC_lyr")
+for v in split_list:
+    print v
+    if split_list.index(v) == 0:
+        where_clause = '"HUC_2" = ' + "'%s'" % v
+        arcpy.SelectLayerByAttribute_management("HUC_lyr", "NEW_SELECTION", where_clause)
+        print('{} has {} records'.format("HUC_lyr", arcpy.GetCount_management("HUC_lyr")[0]))
+    else:
+        where_clause = '"HUC_2" = ' + "'%s'" % v
+        arcpy.SelectLayerByAttribute_management("HUC_lyr", "ADD_TO_SELECTION", where_clause)
+        print('{} has {} records'.format("HUC_lyr", arcpy.GetCount_management("HUC_lyr")[0]))
 
-    #EXPORT LIST OF HUC 12 TO DATAFRAME FROM NHD ATT TABLE
-    array = arcpy.da.TableToNumPyArray(HUC12_path, ['HUC_12'])
-    df = pd.DataFrame(array)
+# RUN SPATIAL JOIN FOR POINT THAT INTERSECT DIRECTLY WITH HUC12S
+# EXPORT LIST OF HUC 12 TO DATAFRAME FROM NHD ATT TABLE
+array = arcpy.da.TableToNumPyArray("HUC_lyr", ['HUC_12'])
+df = pd.DataFrame(array)
+print df
+if not arcpy.Exists(outfc):
+    arcpy.SpatialJoin_analysis("HUC_lyr", "pnt_lyr", outfc, "JOIN_ONE_TO_MANY", "KEEP_ALL", "", "INTERSECT", "", "")
+    print '\nCompleted first spatial join'
 
-    # RUN SPATIAL JOIN FOR POINT THAT INTERSECT DIRECTLY WITH HUC12S
-    if not arcpy.Exists(outfc):
-        arcpy.Delete_management("HUC_lyr")
-        arcpy.MakeFeatureLayer_management(HUC12_path, "HUC_lyr")
-        arcpy.SpatialJoin_analysis("HUC_lyr", "pnt_lyr", outfc, "JOIN_ONE_TO_MANY", "KEEP_ALL", "", "INTERSECT", "", "")
-        print '\nCompleted first spatial join for {0}'.format(value)
+# CHECK FOR HUC12 WITHOUT POINTS WITHIN THE HUC12; SAVE AS SEPARATE FC THEN REMOVES FROM SJ (INTERSECTS) FILE
+if not arcpy.Exists(no_pnt):
+    arcpy.Delete_management("lyr")
+    arcpy.MakeFeatureLayer_management(outfc, "lyr")
+    qry = '"Annual_Prc" IS NULL'
+    # " [Annual_Prc] = 'NULL' "
+    arcpy.SelectLayerByAttribute_management("lyr", "NEW_SELECTION", qry)
+    arcpy.CopyFeatures_management("lyr", no_pnt)
+    fclist_field = [f.name for f in arcpy.ListFields(no_pnt) if not f.required]
+    fclist_field.remove("HUC_12")
+    arcpy.DeleteField_management(no_pnt, fclist_field)
+    print 'Copied no point file'
+    arcpy.DeleteRows_management("lyr")
+    print 'Delete Nulls'
 
-    # CHECK FOR HUC12 WITHOUT POINTS; SAVE AS SEPARATE FC THEN REMOVES FROM SJ (INTERSECTS) FILE
-    if not arcpy.Exists(no_pnt):
-        arcpy.Delete_management("lyr")
-        arcpy.MakeFeatureLayer_management(outfc, "lyr")
-        qry = '"Annual_Prc" IS NULL'
-        # " [Annual_Prc] = 'NULL' "
-        arcpy.SelectLayerByAttribute_management("lyr", "NEW_SELECTION", qry)
-        arcpy.CopyFeatures_management("lyr", no_pnt)
-        fclist_field = [f.name for f in arcpy.ListFields(no_pnt) if not f.required]
-        fclist_field.remove("HUC_12")
-        arcpy.DeleteField_management(no_pnt, fclist_field)
-        print 'Copied no point file'
-        arcpy.DeleteRows_management("lyr")
-        print 'Delete Nulls'
+# RUN SPATIAL JOIN(CLOSEST_GEODESIC) FOR HUC12 W/O POINT DIRECTLY  INSIDE; CLOSEST POINT (ONE TO MANY)
+arcpy.Delete_management("no_pnt_lyr")
+arcpy.MakeFeatureLayer_management(no_pnt, "no_pnt_lyr")
+outfc_2 = closest_point + os.sep + "HUC2_closestPnt"
 
-    # EXPORT RESULTS OF SPATIAL JOIN (INTERSECTS) TO DATAFRAME
-    array_2 = arcpy.da.TableToNumPyArray(outfc, ['HUC_12', 'Annual_Prc', 'WeatherID'])
-    print array_2
-    df_2 = pd.DataFrame(array_2)
 
-    # RUN SPATIAL JOIN(CLOSEST_GEODESIC) FOR HUC12 W/O POINT DIRECTLY  INSIDE; CLOSEST POINT (ONE TO MANY)
-    arcpy.Delete_management("no_pnt_lyr")
-    arcpy.MakeFeatureLayer_management(no_pnt, "no_pnt_lyr")
-    outfc_2 = closest_point + os.sep + "HUC2_" + value + "_closestPnt"
+if not arcpy.Exists(outfc_2):
+    arcpy.SpatialJoin_analysis("no_pnt_lyr", "pnt_lyr", outfc_2, "JOIN_ONE_TO_MANY", "KEEP_ALL", "#",
+                               "CLOSEST_GEODESIC")
+    print 'Completed second spatial join'
 
-    if not arcpy.Exists(outfc_2):
-        arcpy.SpatialJoin_analysis("no_pnt_lyr", "pnt_lyr", outfc_2, "JOIN_ONE_TO_MANY", "KEEP_ALL", "#",
-                                   "CLOSEST_GEODESIC")
-        print 'Completed second spatial join for {0}'.format(value)
+# EXPORT RESULTS OF SPATIAL JOIN (INTERSECTS) TO DATAFRAME Selection on non-nulls - nulls are captured in the no_pnt
+# layer and should be deleted from the layer with the spatial join
+arcpy.MakeFeatureLayer_management(outfc, "lyr")
+qry = '"Annual_Prc" IS NOT NULL'
+arcpy.SelectLayerByAttribute_management("lyr", "NEW_SELECTION", qry)
+array_2 = arcpy.da.TableToNumPyArray("lyr", [u'HUC_12', u'Annual_Prc', u'WeatherID'])
+df_2 = pd.DataFrame(array_2)
+print df_2
 
-    # EXPORT RESULTS OF SPATIAL JOIN (CLOSEST_GEODESIC) TO DATAFRAME
-    array_3 = arcpy.da.TableToNumPyArray(outfc_2, ['HUC_12', 'Annual_Prc', 'WeatherID'])
-    df_3 = pd.DataFrame(array_3)
+# EXPORT RESULTS OF SPATIAL JOIN (CLOSEST_GEODESIC) TO DATAFRAME
+array_3 = arcpy.da.TableToNumPyArray(outfc_2, ['HUC_12', 'Annual_Prc','WeatherID'])
+df_3 = pd.DataFrame(array_3)
+print df_3
 
-    # TODO Update how dfs are merge; this way a number of duplicate rows are be generated when there HUCs are same;
-    # currently removing all duplicates in Summarize script but increasing processing time
+# MERGE RESULTS INTO ONE TABLE AND EXPORT; START WITH ORIGINAL NHD ATT TABLE SO THAT ALL HUC12 ARE ACCOUNTED FOR;
+final_df = pd.merge(df, df_2, on='HUC_12', how='outer')
+out_df = pd.merge(final_df, df_3, on='HUC_12', how='outer')
+# Merges results from the two spatial joins together; only one set of columns is completed x or y so summing then
+# gives the results of all HUCs in a single field
+out_df ['Annual_Prc'] = out_df [['Annual_Prc_x', 'Annual_Prc_y']].sum(axis=1)
+out_df ['WeatherID'] = out_df [['WeatherID_x', 'WeatherID_y']].sum(axis=1)
 
-    # MERGE RESULTS INTO ONE TABLE AND EXPORT; START WITH ORIGINAL NHD ATT TABLE SO THAT ALL HUC12 ARE ACCOUNTED FOR;
-    final_df = pd.merge(df, df_2, on='HUC_12', how='outer')
-    final_df = pd.merge(final_df, df_3, on='HUC_12', how='outer')
-
-    final_df.to_csv(outcsv)
-    print 'Export csv {0}'.format(outcsv)
+out_df.drop_duplicates(inplace=True)  # Remove duplicates from merges
+out_df.to_csv(outcsv)
+print 'Export csv {0}'.format(outcsv)
--- A_Bins/Step_A2_splitHUC2_A_B_Summarize.py
+++ A_Bins/Step_A2_splitHUC2_A_B_Summarize.py
@@ -3,15 +3,15 @@ import os
 import datetime
 
 # outputs from splitHUC2_A_B script
-in_location = r'L:\Workspace\ESA_Species\Step3\ToolDevelopment\AquModeling\SpatialJoins\tables_HUC2'
+in_location = r'D:\Update_AB_splits\SpatialJoins\tables_HUC2'
 # Location where final tables will be saved
-out_location = r'L:\Workspace\ESA_Species\Step3\ToolDevelopment\AquModeling\SpatialJoins\Summarized_spatialJoins'
+out_location = r'D:\Update_AB_splits\SpatialJoins\Summarized_spatialJoins'
 
-# the a/b assigment is fliped for huc 19, a below 20.58 and b is above see email from Chuck 10/20/16- JC
+# the a/b assignment a is above value b is below
+#  the a/b assignment is flipped for huc 19, a below 20.58 and b is above see email from Chuck 10/20/16- JC
 # Values received from Chuck as the a/b break; see email 10/20/16; based off mean annual precip in inches- JC
-
-split_dict = {'10L': 19.75,
-              '10U': 19.75,
+# added HI (20) on 6/11/2018 see email from Chuck
+split_dict = {'10': 19.75,
               '11': 30.26,
               '12': 29.07,
               '15': 15.25,
@@ -19,6 +19,7 @@ split_dict = {'10L': 19.75,
               '17': 30.77,
               '18': 13.74,
               '19': 20.58,
+              '20': 75.94
               }
 
 def createdirectory(new_dir):
@@ -30,94 +31,94 @@ def createdirectory(new_dir):
 
 start_time = datetime.datetime.now()
 print "Start Time: " + start_time.ctime()
+today = datetime.datetime.today()
+date = today.strftime('%Y%m%d')
+
 # get a list of all input files
 list_csv = os.listdir(in_location)
 list_csv = [i for i in list_csv if i.endswith('csv')]
+csv = list_csv[0]
 # create output folder
 createdirectory(out_location)
 all_hucs = pd.DataFrame()
+
+# input table and name of output table
+in_csv = in_location + os.sep + list_csv[0]  # with seamless data from NHDplus only one csv
+out_csv = out_location + os.sep + csv
+
+# read in input and drop index column
+in_df = pd.read_csv(in_csv, dtype=object)
+in_df  = in_df .loc[in_df ['HUC_12'] != 'None']
+in_df.drop(labels=['Unnamed: 0'], axis=1, inplace=True)
+
+# assigns float data type to number and str to HUC12; csv assume data types
+in_df['Annual_Prc'] = in_df['Annual_Prc'].map(lambda x: x).astype(float)
+in_df['WeatherID'] = in_df['WeatherID'].map(lambda x: x).astype(float)
+in_df['HUC_12'] = in_df['HUC_12'].map(lambda x: str(x) if len(str(x)) == 12 else '0' + str(x)).astype(str)
+
+# Extract columns to be used for summary stats
+total_precip = in_df[['HUC_12', 'Annual_Prc','WeatherID']]
+# sets output files name for intermediate file
+total_outcsv = r'D:\Update_AB_splits\SpatialJoins\total_precip'+ os.sep + csv
+
+# removed duplicate values
+# keep WeatherID until after remove duplicates so that only true duplicates are removed; ie two point in a HUC12
+# may have the annual average  precip and those should NOT be remved
+total_precip = total_precip.drop_duplicates()
+
+# saves intermediate file
+total_precip.to_csv(total_outcsv)
+#total_precip.drop(labels=['WeatherID'], axis=1, inplace=True)  excluded from table in previous step
+total_precip['HUC_2'] = total_precip['HUC_12'].map(lambda x: x[:2]).astype(str)
+
+# Loop through each huc2 with split and assigned the a or b based on the info in split_dict
 # Generate summary stats (mean, max, min) for each input file;assign a/b based on values in split dict; then save output
-for csv in list_csv:
-    print csv
-    # Current HUC2 values, extracted from input csv name; used to extract split value from split_dict
-    huc = str(csv.split("_")[1])
-    huc = huc.replace('.csv', '')
-    split = split_dict[huc]
+for huc2 in split_dict.keys():
+    split = split_dict[huc2]
 
-    # input table and name of output table
-    in_csv = in_location + os.sep + csv
-    out_csv = out_location + os.sep + csv
-
-    # read in input and drop index column
-    in_df = pd.read_csv(in_csv, dtype=object)
-    in_df.drop(labels=['Unnamed: 0'], axis=1, inplace=True)
-
-    # assigns float data type; csv assume data types
-    in_df['Annual_Prc_x'] = in_df['Annual_Prc_x'].map(lambda x: x).astype(float)
-    in_df['Annual_Prc_y'] = in_df['Annual_Prc_y'].map(lambda x: x).astype(float)
-    in_df['WeatherID_x'] = in_df['WeatherID_x'].map(lambda x: x).astype(float)
-    in_df['WeatherID_y'] = in_df['WeatherID_y'].map(lambda x: x).astype(float)
-
-    # Merges results from the two spatial joins together; only one set of columns is completed x or y so summing then
-    # gives the results of all HUCs in a single field
-    in_df['Annual_Prc'] = in_df[['Annual_Prc_x', 'Annual_Prc_y']].sum(axis=1)
-    in_df['WeatherID'] = in_df[['WeatherID_x', 'WeatherID_y']].sum(axis=1)
-
-    # Extract columns to be used for summary stats
-    total_precip = in_df[['HUC_12', 'Annual_Prc', 'WeatherID']]
-    print total_precip.columns.values.tolist()
-    # sets output files name for intermediate file
-    total_outcsv = r'L:\Workspace\ESA_Species\Step3\ToolDevelopment\AquModeling\SpatialJoins\total_precip' \
-                   + os.sep + csv
-    print len(total_precip)
-    # removed duplicate values see TODO in splitHUC2_A_B;
-    # keep WeatherID until after remove duplicates so that only true duplicates are removed; ie two point in a HUC12
-    # may have theannual average  precip and those should NOT be remved
-
-    total_precip = total_precip.drop_duplicates()
-    print len(total_precip)
-    # saves intermediate file
-    total_precip.to_csv(total_outcsv)
-    total_precip.drop(labels=['WeatherID'], axis=1, inplace=True)
-
+    c_huc2 = total_precip.loc[(total_precip['HUC_2'] == huc2)]
+    c_huc2 = c_huc2.ix[:,['HUC_12','Annual_Prc']]
     # generate summary stats as series in a groupby
-    att_max = total_precip.groupby(by=['HUC_12']).max()
-    att_min = total_precip.groupby(by=['HUC_12']).min()
-    att_mean = total_precip.groupby(by=['HUC_12']).mean()
-    att_count = total_precip.groupby(by=['HUC_12']).count()
+    att_max = c_huc2.groupby(by=['HUC_12']).max()
+    att_min = c_huc2.groupby(by=['HUC_12']).min()
+    att_mean = c_huc2.groupby(by=['HUC_12']).mean()
+    att_count = c_huc2.groupby(by=['HUC_12']).count()
 
     # export each summary to a data frame sorted by HUC12 values (str)
-    # TODO to speed this up; export direcly to final df and elimate merge at the end
-    # TODO set up function get dynamically generate a suite of summary stats and split based on user inputed list;
+    # TODO could export directly to final df and elimate merge at the end
     count_df = pd.DataFrame(att_count)
     count_df.reset_index(level=0, inplace=True)
-    count_df['HUC_12'] = count_df['HUC_12'].map(lambda x: x).astype(str)
+    count_df = count_df.drop_duplicates()
+
     count_df.sort_values(['HUC_12'])
     count_df.columns = ['HUC_12', 'Count']
 
     max_df = pd.DataFrame(att_max)
     max_df.reset_index(level=0, inplace=True)
-    max_df['HUC_12'] = max_df['HUC_12'].map(lambda x: x).astype(str)
+    max_df = max_df.drop_duplicates()
+
     max_df.sort_values(['HUC_12'])
     max_df.columns = ['HUC_12', 'Max']
 
     min_df = pd.DataFrame(att_min)
     min_df.reset_index(level=0, inplace=True)
-    min_df['HUC_12'] = min_df['HUC_12'].map(lambda x: x).astype(str)
+    min_df = min_df.drop_duplicates()
+
     min_df.sort_values(['HUC_12'])
     min_df.columns = ['HUC_12', 'Min']
 
     mean_df = pd.DataFrame(att_mean)
     mean_df.reset_index(level=0, inplace=True)
-    mean_df['HUC_12'] = mean_df['HUC_12'].map(lambda x: x).astype(str)
+    mmean_df = mean_df.drop_duplicates()
+
     mean_df.sort_values(['HUC_12'])
     mean_df.columns = ['HUC_12', 'Mean']
     mean_df['Mean'] = mean_df['Mean'].map(lambda x: "{0:.2f}".format(x))
 
     # see note at dict to explain what these are different
     # Checks the mean value for each HUC12 and assigned a or b based on values in split_dict
-    if huc == '19':
-        mean_df['HUC_Split'] = mean_df['Mean'].map(lambda x: 'b' if float(x) > split else 'a')
+    if huc2 == '19':
+        mean_df['HUC_Split'] = mean_df['Mean'].map(lambda x: 'b' if float(x) >= split else 'a')
     else:
         mean_df['HUC_Split'] = mean_df['Mean'].map(lambda x: 'a' if float(x) > split else 'b')
 
@@ -128,4 +129,5 @@ for csv in list_csv:
     final_df.to_csv(out_csv)
     all_hucs = pd.concat([all_hucs,final_df], axis=0)
 
-all_hucs.to_csv(r'L:\Workspace\ESA_Species\Step3\ToolDevelopment\AquModeling\SpatialJoins\Summarized_spatialJoins\AllHUC_a_b.csv')
+all_hucs['HUC_2'] = all_hucs['HUC_12'].map(lambda x: x[:2]).astype(str)
+all_hucs.to_csv(r'D:\Update_AB_splits\SpatialJoins\AllHUC_a_b_' + date+'.csv')
