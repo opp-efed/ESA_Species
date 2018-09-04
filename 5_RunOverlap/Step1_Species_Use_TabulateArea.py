864291521d0c4f3fe1ddd4bf153a7c85853121b5
jconnolly814 <814pihtaz05!!>
2018-09-04 10:47:40 -0400

    Merge branch 'Jen' of https://github.com/opp-efed/ESA_Species
    # Conflicts:
    #	.idea/CleanScripts_QA.iml
    #	.idea/misc.xml

--- 5_RunOverlap/Step1_Species_Use_TabulateArea.py
+++ 5_RunOverlap/Step1_Species_Use_TabulateArea.py
@@ -41,29 +41,12 @@ skip_species = []
 # skip_species = []
 in_location_species_folder = 'CONUS_Albers_Conical_Equal_Area'
 
-temp_file = "temp_table_91"  # Should not use the same temp file name when running multiple instances at the same time
+temp_file = "temp_table_93"  # Should not use the same temp file name when running multiple instances at the same time
 run_group = 'UseLayers'  # UseLayers, Yearly, OnOffField
 
 # Manually sub-set layers to be run: complete region run faster by splitting run into several instances
 
-use_list = [u'Albers_Conical_Equal_Area_CDL_1016_100x2_euc', u'Albers_Conical_Equal_Area_CDL_1016_70x2_euc',
-            u'Albers_Conical_Equal_Area_CDL_1016_71x2_euc', u'Albers_Conical_Equal_Area_CDL_1016_40x2_euc',
-            u'Albers_Conical_Equal_Area_CDL_1016_10x2_euc', u'Albers_Conical_Equal_Area_CDL_1016_80x2_euc',
-            u'Albers_Conical_Equal_Area_CDL_1016_72x2_euc', u'Albers_Conical_Equal_Area_CDL_1016_20x2_euc',
-            u'Albers_Conical_Equal_Area_CDL_1016_90x2_euc', u'Albers_Conical_Equal_Area_CDL_1016_60x2_euc',
-            u'Albers_Conical_Equal_Area_CDL_1016_30x2_euc', u'Albers_Conical_Equal_Area_CONUS_OSD_euc',
-            u'Albers_Conical_Equal_Area_CONUS_Developed_euc', u'Albers_Conical_Equal_Area_CONUS_FederalLands_euc',
-            u'Albers_Conical_Equal_Area_CONUS_methomyl_171227_AA_euc',
-            u'Albers_Conical_Equal_Area_CONUS_carbaryl_180410_AA_euc',
-            u'Albers_Conical_Equal_Area_CONUS_carbaryl_180410_AA_nonAg_euc',
-            u'Albers_Conical_Equal_Area_CONUS_Ndev_ROW_180306_euc',
-
-            u'Albers_Conical_Equal_Area_CONUS_ManagedForests_xmas_180307_euc',
-            u'Albers_Conical_Equal_Area_CONUS_Methomyl_CONUS_bermudagrass2_euc',
-
-            u'Albers_Conical_Equal_Area_CONUS_methomyl_citrus_171227_euc',
-            u'Albers_Conical_Equal_Area_CONUS_Methomyl_alleycropping2_euc',
-            u'Albers_Conical_Equal_Area_CONUS_methomyl_wheat_171227_euc', u'Albers_Conical_Equal_Area_CDL_1016_110_euc']
+use_list = []
 # [u'Albers_Conical_Equal_Area_CONUS_methomyl_171227_AA_euc', u'Albers_Conical_Equal_Area_CONUS_carbaryl_180410_AA_euc', u'Albers_Conical_Equal_Area_CONUS_carbaryl_180410_AA_nonAg_euc', u'Albers_Conical_Equal_Area_CONUS_Ndev_ROW_180306_euc', u'Albers_Conical_Equal_Area_CONUS_methomyl_171227_AA_ag_euc', u'Albers_Conical_Equal_Area_CONUS_ManagedForests_xmas_180307_euc', u'Albers_Conical_Equal_Area_CONUS_Methomyl_CONUS_bermudagrass2_euc', u'Albers_Conical_Equal_Area_CONUS_carbaryl_171227d_AA_ag_euc', u'Albers_Conical_Equal_Area_CONUS_methomyl_citrus_171227_euc', u'Albers_Conical_Equal_Area_CONUS_Methomyl_alleycropping2_euc', u'Albers_Conical_Equal_Area_CONUS_methomyl_wheat_171227_euc', u'Albers_Conical_Equal_Area_CDL_1016_110_euc']
 # ################Static variables
 arcpy.CheckOutExtension("Spatial")
@@ -191,16 +174,19 @@ def zonal_hist(sp_path, in_value_raster, region_c, use_name, temp_table, final_f
         arcpy.MakeRasterLayer_management(Raster(sp_path), "zone")
         print "   loaded species in  {0}".format((datetime.datetime.now() - start_species))
         temp_return, zone_time = zone("zone", in_value_raster, temp_table, snap)
+        arcpy.Delete_management("zone")  # deleted the specie raster layer in memory
 
         list_fields = [f.name for f in arcpy.ListFields(temp_return)]
         att_array = arcpy.da.TableToNumPyArray(temp_return, list_fields)
+        arcpy.Delete_management(temp_return)  # delete table in memory after saving - frees up memory
         att_df = pd.DataFrame(data=att_array)
+        del att_array
         att_df['VALUE'] = att_df['VALUE'].map(lambda x: x).astype(str)
+        # print att_df.info()  # provided details ont he size of the DF after setting values to str
         att_df.to_csv(out_path_final + os.sep + csv)
         print '   Final file can be found at {0}'.format(out_path_final + os.sep + csv)
         print "   Completed in {0}\n".format((datetime.datetime.now() - zone_time))
-        arcpy.Delete_management("zone")
-        arcpy.Delete_management(temp_return)  # delete table in memory after saving - frees up memory
+
         del att_df  # deletes df from memory after saving
 
 
